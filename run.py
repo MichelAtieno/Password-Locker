c81894c4634d1f837059f3f7f2312f615e134f18
Michel Atieno <mishqamish@gmail.com>
2018-08-26 22:23:32 +0300

    Removed errors

--- run.py
+++ run.py
@@ -89,24 +89,28 @@ def main():
         selected_type = input().lower().strip()
         if selected_type == 'ca':
             print('Create Account:'+'\n'+'-'*25+'\n Enter Account-Name:')
-            new_first_name = input('Account-First-Name: ')
-            new_last_name = input('Account-Last-Name: ')
+            new_first_name = input('Account-First-Name: ').strip()
+            new_last_name = input('Account-Last-Name: ').strip()
+
             print('Enter email: ')
-            new_email = input('Account-Email: ')
+            new_email = input('Account-Email: ').strip()
+
             print('Enter password: ')
-            pass_word = input('Password: ')
+            pass_word = input('Password: ').strip()
 
             print('\n')
             save_account(create_account(my_id,new_first_name,new_last_name,new_email,pass_word))
-            my_id+=1 
+            my_id+=1
+
             print(f'Account {new_first_name} {new_last_name} has been created. \n Login to Continue')
             user_input.append(0)
+
             print('-'*25)
 
         elif selected_type == 'si':
             print('Enter Account-Name and Password to continue:')
-            user_login = input('Account-Name: ')
-            user_webpass = input('Password: ')
+            user_login = input('Account-Name: ').strip()
+            user_webpass = input('Password: ').strip()
             user_signin = authenticate_account(user_login,user_webpass)
             if user_signin == 0:
                 print('\n')
@@ -115,7 +119,7 @@ def main():
 
             elif user_signin != 0:
                 print('\n')
-                print('Welcome {user-signin.first_name}')
+                print(f'Welcome {user_signin.first_name}')
                 while True:
                     print('Type:\n cp - Create Password sp- Show Passwords cop - Copy Password to clipboard\n lo - Log out')
                     new_user_input = input().lower()
@@ -124,10 +128,10 @@ def main():
                         print('Add Website and Password: ')
                         print('Enter Website: ')
                         new_website = input()
+
                         print('Length of Password: ')
                         password_length = int(input('Password Length: '))
                         new_webpass = webpass_generator(password_length)
-
                         new_user_id = user_signin.user_id
                         save_userdata(my_new_userdata(new_user_id,user_input[new_user_id],new_website,new_webpass))
                         user_input[new_user_id] = user_input[new_user_id]+1
@@ -143,7 +147,7 @@ def main():
                          my_user_data=0
                          while my_user_data < length:
                              get_pass = display_userdata(user_signin.user_id,my_user_data)
-                             print(f'{my_user_data+1}. {get_pass.website} ---- {get_pass.webpass}')
+                             print(f'{my_user_data+1}. Your Password for {get_pass.website} is {get_pass.webpass}')
                              my_user_data+=1
                     
 
@@ -159,9 +163,11 @@ def main():
                          show_index = int(input('Enter index: '))-1
                          if show_index >= user_input[user_signin.user_id] or show_index<0:
                              print('\n')
+
                              print(f'{show_index+1} is invalid')
                              print('Enter sp to verify index of password')
                              print('-'*25)
+
                          elif show_index <user_input[user_signin.user_id]:
                                  copy_pass(user_signin.user_id,show_index)
                                  print('\n')
